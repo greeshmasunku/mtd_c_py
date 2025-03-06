1dbd381b05f6d9d75191724ea354b90be80927b3
greeshmasunku <greeshma.sunkam@gmail.com>
2025-03-06 14:36:52 +0530

    git commit -m "file added"

---
+++ d1/p1.c
@@ -0,0 +1,13 @@
+// Accept a number from the user and check if it is Even.
+#include<stdio.h>
+int main()
+{
+    int inputNum = 0;
+    puts("Enter a number to check it is Even");
+    scanf("%d", &inputNum);
+    if(inputNum % 2 == 0)
+        printf("%d is Even", inputNum);
+    else
+        printf("%d is not Even", inputNum);
+    return 0;
+}
---
+++ d1/p2.c
@@ -0,0 +1,13 @@
+// Accept a number from the user and check if it is Even.
+#include<stdio.h>
+int main()
+{
+    int num1 = 0;
+    long int num2 = 0;
+    long long int num3 = 0;
+    double d = 0.0;
+    float f = 0.0f;
+    printf("%d %d %d %d \n", sizeof(int),
+    sizeof(long int), sizeof(long long int), sizeof(double), sizeof(float));
+    return 0;
+}
